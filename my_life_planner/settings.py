f3bdd119027c08c7a07a70c3da73e9ba4bca7536
AnnikaW-dev <warmannika@gmail.com>
2025-08-08 13:31:21 +0200

    Change SECRET_KEY & remove from settings.py & set DEBUG to false

--- my_life_planner/settings.py
+++ my_life_planner/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3#88q237=nvus@3!2mth%g=y061=@uwt%zhzk!#e!r2zen1m5'
+SECRET_KEY = os .environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # vs code preview
+    'localhost',  # Listen for stripe webhooks
+]
 
 
 # Application definition
